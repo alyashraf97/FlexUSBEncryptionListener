9d15b468e8bb3cf476235a2c2eab1676645b4d8a
alyashraf97 <captainslowg@gmail.com>
2023-05-08 20:11:58 +0300

    Add project files.

---
+++ LICENSE.md
@@ -0,0 +1,8 @@
+﻿Copyright (c) 2023 Aly M. Ghobashy
+All rights reserved.
+
+This software is the property of Aly M. Ghobashy (who is the sole proprietor and author of this source code) and may not be copied, distributed, or modified without explicit permission from the Author of the code (Aly M. Ghobashy).
+
+Use of this software is permitted only for the purposes and under the conditions explicitly stated by Aly M. Ghobashy. Unauthorized use, modification, or distribution of this software is strictly prohibited, and may be subject to legal action.
+
+This software is provided "AS IS", without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose, and noninfringement. In no event shall Aly M. Ghobashy be liable for any claim, damages, or other liability, whether in an action of contract, tort, or otherwise, arising from, out of, or in connection with the software or the use or other dealings in the software.
---
+++ SleeperService.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SleeperService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a socket and bind it to the port that Symantec DLP FlexResponse will use to communicate with the sleeper service.
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(new IPEndPoint(IPAddress.Any, 5000));
+            socket.Listen(10);
+
+            // Listen for incoming connections.
+            while (true)
+            {
+                // Accept an incoming connection.
+                Socket clientSocket = socket.Accept();
+
+                // Read the request from the client.
+                byte[] request = new byte[1024];
+                int bytesRead = clientSocket.Receive(request);
+
+                // Decode the request.
+                string requestString = Encoding.UTF8.GetString(request, 0, bytesRead);
+
+                // Parse the request.
+                var requestData = JsonConvert.DeserializeObject<RequestData>(requestString);
+
+                // Check the request type.
+                if (requestData.RequestType == "EncryptFile")
+                {
+                    // Encrypt the file.
+                    string encryptedFilePath = EncryptFile(requestData.FilePath);
+
+                    // Send the encrypted file path back to the client.
+                    byte[] responseBytes = Encoding.UTF8.GetBytes(encryptedFilePath);
+                    clientSocket.Send(responseBytes);
+                }
+
+                // Close the client socket.
+                clientSocket.Close();
+            }
+        }
+
+        private static string EncryptFile(string filePath)
+        {
+            // Load the encryption key.
+            byte[] encryptionKey = GetEncryptionKey();
+
+            // Open the file to be encrypted.
+            FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            // Get the encrypted file path.
+            string encryptedFilePath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".encrypted");
+
+            // Create a crypto stream for encrypting the file.
+            using (CryptoStream cryptoStream = new CryptoStream(new FileStream(encryptedFilePath, FileMode.Create), new AesManaged(), CryptoStreamMode.Write, true))
+            {
+                // Encrypt the file.
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    cryptoStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            // Close the file stream.
+            fileStream.Close();
+
+            // Return the encrypted file path.
+            return encryptedFilePath;
+        }
+
+        private static byte[] GetEncryptionKey()
+        {
+            // Load the encryption key from the configuration file.
+            var configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+            var encryptionKeySection = configuration.GetSection("EncryptionKey");
+            var encryptionKey = encryptionKeySection.Settings["Key"].Value;
+
+            // Convert the encryption key to a byte array.
+            byte[] encryptionKeyBytes = Encoding.UTF8.GetBytes(encryptionKey);
+
+            // Return the encryption key bytes.
+            return encryptionKeyBytes;
+        }
+    }
+
+    [JsonObject]
+    class RequestData
+    {
+        [JsonProperty("RequestType")]
+        public string RequestType { get; set; }
+
+        [JsonProperty("FilePath")]
+        public string FilePath { get; set; }
+    }
+}
